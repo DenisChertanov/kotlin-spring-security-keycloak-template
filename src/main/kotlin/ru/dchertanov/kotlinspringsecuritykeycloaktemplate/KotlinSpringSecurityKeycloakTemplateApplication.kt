84f12e687e4b2e188665ad81d0313a23c0d8e4b3
DenisChertanov <dachertanov@edu.hse.ru>
2022-07-20 13:40:05 +0300

    Init commit

---
+++ src/main/kotlin/ru/dchertanov/kotlinspringsecuritykeycloaktemplate/KotlinSpringSecurityKeycloakTemplateApplication.kt
@@ -0,0 +1,11 @@
+package ru.dchertanov.kotlinspringsecuritykeycloaktemplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringSecurityKeycloakTemplateApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringSecurityKeycloakTemplateApplication>(*args)
+}
---
+++ src/test/kotlin/ru/dchertanov/kotlinspringsecuritykeycloaktemplate/KotlinSpringSecurityKeycloakTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.dchertanov.kotlinspringsecuritykeycloaktemplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringSecurityKeycloakTemplateApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
